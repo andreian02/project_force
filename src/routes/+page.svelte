75e03b96afbe76176dc8832542007e1664ed7319
Andrew Lim <andrew.lim@SGPOGV0026MAC.local>
2023-04-21 21:49:16 +0800

    4th commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,29 @@
+<script>
+
+	//import NetworkGraphCanvas from './NetworkGraphCanvas.svelte';
+	//import GraphSvelteSVG from './NetworkGraphSvelteSVG.svelte';
+	import NetworkGraphD3SVG from './force/NetworkGraphD3SVG.svelte';
+	import data from './force/data2.js';
+	
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 
-<a href="/force">Graph</a>
+
+<div class="chart">
+	<NetworkGraphD3SVG graph={data} />
+</div>
+
+
+
+
+<style>
+	.chart {
+		width: max-content;
+		height: max-content;
+		margin: 0 auto;
+	}
+</style>
